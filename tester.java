3a29250bceee4d38f47ce3c6476fa5d4def86fbd
kenyawest <judegantony@gmail.com>
2018-03-25 00:13:50 -0400

    trying

---
+++ tester.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
